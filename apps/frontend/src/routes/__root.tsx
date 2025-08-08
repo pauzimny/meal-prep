d2b049cee4df3ba0a21ddd78713c0803dd39f11b
Paulina Zimny <zimny.paulina@gmail.com>
2025-08-08 12:20:03 +0200

    Fix vite config

--- apps/frontend/src/routes/__root.tsx
+++ apps/frontend/src/routes/__root.tsx
@@ -21,7 +21,7 @@ function RootComponent() {
     <div className="w-full min-h-[100vh] flex flex-col">
       {!!user && (
         <div className="sticky top-0 z-10">
-          <Heading email={user.email!} name={userProfile!.name!} />
+          <Heading email={user.email!} name={userProfile?.name} />
           <div className="py-4 px-6 flex gap-6 bg-white">
             <NavLink to="/" activeProps={{}} activeOptions={{ exact: true }}>
               Home
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+// @ts-expect-error
+import tailwindcssVite from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwindcssVite()],
 });

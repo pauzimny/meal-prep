a624c649d2fc4785e1ff579d6eb9081d90e2811b
Paulina Zimny <zimny.paulina@gmail.com>
2025-08-08 12:41:08 +0200

    Display saved meals as accordion

--- apps/frontend/src/components/SavedMeals/SavedMeals.tsx
+++ apps/frontend/src/components/SavedMeals/SavedMeals.tsx
@@ -1,11 +1,21 @@
 import { Link } from "@tanstack/react-router";
+import * as Accordion from "@radix-ui/react-accordion";
 import { useUserStore } from "../../stores/userStore";
 import { MealSuggestionResult } from "../MealSuggestionResult/MealSuggestionResult";
+import { useState } from "react";
 
 export function SavedMeals() {
   const userProfile = useUserStore((state) => state.user);
 
+  const [openItems, setOpenItems] = useState<string[]>([]);
+
   const mealsList = userProfile?.saved_meals;
+
+  const handleHeaderClick = (value: string) => {
+    setOpenItems((prev) =>
+      prev.includes(value) ? prev.filter((v) => v !== value) : [...prev, value]
+    );
+  };
 
   if (!mealsList || mealsList.length === 0) {
     return (
@@ -17,10 +27,37 @@ export function SavedMeals() {
   }
 
   return (
-    <ul>
-      {mealsList?.map((meal) => (
-        <MealSuggestionResult {...meal} key={meal.title} />
-      ))}
-    </ul>
+    <Accordion.Root
+      type="multiple"
+      value={openItems}
+      onValueChange={setOpenItems}
+      className="w-full"
+    >
+      {mealsList.map((meal, idx) => {
+        const value = meal.title + idx;
+        return (
+          <Accordion.Item
+            value={value}
+            key={value}
+            className="border rounded my-2"
+          >
+            <Accordion.Header
+              className="cursor-pointer"
+              onClick={() => handleHeaderClick(value)}
+            >
+              <Accordion.Trigger
+                className="w-full text-left text-primary px-4 py-2 font-semibold bg-white transition hover:bg-primary/10"
+                onClick={(e) => e.preventDefault()}
+              >
+                {meal.title}
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="p-4">
+              <MealSuggestionResult {...meal} />
+            </Accordion.Content>
+          </Accordion.Item>
+        );
+      })}
+    </Accordion.Root>
   );
 }
